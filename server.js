39b22b6f28b2b02834823c0f68915dfb82f9c15d
DhudsQ <joseasto24@gmail.com>
2026-05-13 19:17:42 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const jsonServer = require('json-server');
+  const server = jsonServer.create();
+  const router = jsonServer.router('db.json');
+  const middlewares = jsonServer.defaults();
+  
+  const PORT = process.env.PORT || 10000;
+
+  server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') return res.sendStatus(200);
+    next();
+  });
+
+  server.use(middlewares);
+  server.use(router);
+
+  server.listen(PORT, '0.0.0.0', () => {
+    console.log(`FullTank JSON Server running on port ${PORT}`);
+  });
